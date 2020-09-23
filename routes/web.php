3453ba7cd214bcf0e912fd73b218bd267e53e91b
bilalsaleemdev <bilalsaleem.dev@gmail.com>
2020-09-23 12:14:43 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'success' => true,
-        'message' => 'Welcome to dlivrdChatServer'
+        'message' => 'Welcome to dlivrd Chat Server'
     ]);
 });
